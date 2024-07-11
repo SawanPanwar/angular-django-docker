94bb2aab3ed4f357b062c7a78fd70cb499f8b582
SawanPanwar <skpanwar25@gmail.com>
2024-07-11 18:52:00 +0530

    done

---
+++ angular/src/app/http-service.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  get(endpoint: any, callback: any) {
+    return this.httpClient.get(endpoint).subscribe((data: any) => {
+      callback(data)
+    }, (error: any) => {
+      callback(error)
+    })
+  }
+
+  post(endpoint: any, bean: any, callback: any) {
+    return this.httpClient.post(endpoint, bean).subscribe((data: any) => {
+      callback(data)
+    })
+  }
+}
